a46b33ccd5f39a75aa60cbf82f40cb601b12d8f6
helxvex <104658832+helxvex@users.noreply.github.com>
2022-09-08 10:13:00 -0500

    add work

---
+++ src/main/kotlin/week02/income.kt
@@ -0,0 +1,8 @@
+fun main (){
+   var name = "juan"
+    var age = 39
+   var annualPay = 100000.0
+
+    println("My name is " + name + " , my age is "+age+" and")
+    println("I hope to earn "+annualPay+" per year.")
+}
