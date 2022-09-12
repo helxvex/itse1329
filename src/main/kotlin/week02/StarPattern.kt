706b712d8533a11218a9de60ed9d681f3ac9debe
helxvex <104658832+helxvex@users.noreply.github.com>
2022-09-11 22:02:57 -0500

    StarPattern

---
+++ src/main/kotlin/week02/StarPattern.kt
@@ -0,0 +1,12 @@
+package week02
+
+
+    fun main(args: Array<String>) {
+        println("...*\n" +
+            "..***\n" +
+            ".*****\n" +
+            "*******\n" +
+            ".*****\n" +
+            "..***\n" +
+            "...* ")
+    }
