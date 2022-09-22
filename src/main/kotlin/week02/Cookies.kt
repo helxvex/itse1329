ac4699d21f3ed56c84f7a378053a280b2181af82
helxvex <104658832+helxvex@users.noreply.github.com>
2022-09-22 08:53:55 -0500

    some class work from week 2 through 4.

--- src/main/kotlin/week02/Cookies.kt
+++ src/main/kotlin/week02/Cookies.kt
@@ -2,9 +2,8 @@ package week02
 import java.util.Scanner
 // Florencio rueda  2.7 Cookie Calorie Calculator 09/15/22
 fun main(args: Array<String>){
-    //val scanner = Scanner(System.`in`)
     println("How many cookies have you ate?");
-    var totalCookiesAte= Integer.valueOf(readLine())//scanner.nextInt();
+    var totalCookiesAte= Integer.valueOf(readLine())
     var caloriesInEach = 300/4;
     var totalCalories = totalCookiesAte*caloriesInEach;
     println("Total calories comsumed: $totalCalories");
