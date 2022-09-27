d97f202c27962ca59df10bfbf04a3d7c2d8c841c
helxvex <104658832+helxvex@users.noreply.github.com>
2022-09-27 16:28:06 -0500

    code into kotlin

--- src/main/kotlin/week04/Population.kt
+++ src/main/kotlin/week04/Population.kt
@@ -1,22 +1,36 @@
 package week04
-// Florencio rueda 4.3 Population 09/25/22
 
-    fun main(args: Array<String>) {
-        var rate = 0.0
-        print("Number of organisms: ")
-        var numOfOrganisms = Integer.valueOf(readLine())
+// Florencio rueda 4.3 Population 09/27/22
 
-        print("Number of days to multiply: ")
-        var days = Integer.valueOf(readLine())
+fun main(args: Array<String>) {
 
-        print("Percentage of increase: ")
-        var percentage = Integer.valueOf(readLine())/ 100
 
-        println("")
-        println("Day" + "       Population" + "       Percent of Increase")
-        println("  1           "+ numOfOrganisms)
-        for (i in 2 until days + 1) {
-            numOfOrganisms += 0.let { rate *= it; numOfOrganisms }
-            println("  " + i + "           " + numOfOrganisms + "                " +  (rate* 100 ) + "%")
-        }
+    var days: Int
+    var organisms = 0.0
+    var increaseRate = 0.0
+    print("Number of organisms: ")
+    organisms = readLine()!!.toDouble()
+    while (organisms < 2) {
+        print("Invalid entry. Re-enter a value greater than 1: ")
+        organisms = readLine()!!.toDouble()
     }
+    print("Number of days to multiply: ")
+    days = Integer.valueOf(readLine())
+    while (days < 1) {
+        print("Invalid entry. Re-enter a value greater than 0: ")
+        days = Integer.valueOf(readLine())
+    }
+    print("Percentage of increase: ")
+    increaseRate = readLine()!!.toDouble() / 100
+    while (increaseRate < 0) {
+        print("Invalid entry. Re-enter a non-negative value: ")
+        increaseRate = readLine()!!.toDouble()
+    }
+    println("")
+    println("Day" + "\tPopulation" + "\tPercent of Increase")
+    println("\t1 \t" + organisms)
+    for (i in 2 until days + 1) {
+        organisms += increaseRate.let { organisms *= it; organisms }
+        println("\t" + i + "\t" + Math.ceil(organisms) + "    " + increaseRate * 100 + "%")
+    }
+}
