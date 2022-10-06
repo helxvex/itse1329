be2d994e2551df9a6e7f74e9bd67f39f3b6d489b
helxvex <104658832+helxvex@users.noreply.github.com>
2022-10-06 10:23:42 -0500

    "fix"

--- src/main/kotlin/week02/Cookies.kt
+++ src/main/kotlin/week02/Cookies.kt
@@ -1,5 +1,5 @@
 package week02
-import java.util.Scanner
+
 // Florencio rueda  2.7 Cookie Calorie Calculator 09/15/22
 fun main(args: Array<String>){
     println("How many cookies have you ate?");
