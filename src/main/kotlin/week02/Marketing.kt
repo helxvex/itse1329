ec4b7a01f4863aad1d917502a7cce148cf5c0d55
helxvex <104658832+helxvex@users.noreply.github.com>
2022-10-13 10:45:22 -0500

    add to folder

---
+++ src/main/kotlin/week02/Marketing.kt
@@ -0,0 +1,6 @@
+fun main () {
+    var friendsCount = 430
+    if (friendsCount>500) println("Adopt a Dog Today!")
+    else println( "Buy Cat Food At 20% Off")
+    println("You have " + friendsCount + " friends")
+}
---
+++ src/main/kotlin/week02/VariableDeclaration.kt
@@ -0,0 +1,5 @@
+
+var lucky = '8'
+var eachDay = 1.0
+var age = 88
+var kotlinRocks = true
